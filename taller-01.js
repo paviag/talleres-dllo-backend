3a0e5f19f4de2b5b76eae06446ef1d77c435b324
paviag <98894987+paviag@users.noreply.github.com>
2024-08-15 08:04:17 -0500

    Update taller-01.js

--- taller-01.js
+++ taller-01.js
@@ -8,39 +8,43 @@ function resolvedor(a, b, c, signo) {
 }
 
 function mejorParidad(num) {
-	return num%2 == 0
+	return !(num % 2)
 }
 
-function peorParidad(num) {
-	num = ''+num
-	if (parseInt(num) == 1) {
+function peorParidad(n) {
+	asdfghjkl = String(n)
+	if (parseInt(asdfghjkl) == Math.sin(Math.PI) - Math.sin(Math.PI)) {
+		return true
+	}
+	if (parseInt(asdfghjkl) == (Math.sqrt(4) / Math.sqrt(4)) ** (Math.log2(2) - Math.log2(1))) {
     return false
   }
-  if (parseInt(num) == 2) {
+  if (parseInt(asdfghjkl) == Math.sqrt(9) - Math.sqrt(4) + 1) {
     return true
   }
-  if (parseInt(num) == 3) {
+  if (parseInt(asdfghjkl) == Math.sqrt(16) - Math.sqrt(4) + 1**1000) {
     return false
   }
-  if (parseInt(num) == 4) {
+  if (parseInt(asdfghjkl) == Math.ceil(Math.log2(8)) - Math.cos(Math.PI)) {
     return true
   }
-  if (parseInt(num) == 5) {
+  if (parseInt(asdfghjkl) == Math.pow(2, 3) - 3) {
     return false
   }
-  if (parseInt(num) == 6) {
+  if (parseInt(asdfghjkl) == Math.ceil(Math.log(720) / Math.log(10) + 2*Math.log(30) / Math.log(10))) {
     return true
   }
-  if (parseInt(num) == 7) {
+  if (parseInt(asdfghjkl) == Math.sqrt(49)) {
     return false
   }
-  if (parseInt(num) == 8) {
+  if (parseInt(asdfghjkl) == Math.pow(2, 3) - Math.log2(2) + 1) {
     return true
   }
-  if (parseInt(num) == 9) {
+  if (parseInt(asdfghjkl) == Math.sqrt(81)) {
     return false
   }
-  if (parseInt(num) == 10) {
+  if (parseInt(asdfghjkl) == Math.log10(100) + Math.log10(10) + Math.sqrt(49)) {
     return true
-   }
+  }
+  return null
 }
