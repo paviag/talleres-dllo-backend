2214665b56be468b7a5bea739175f47543dd4477
paviag <98894987+paviag@users.noreply.github.com>
2024-08-30 20:39:03 -0500

    Update taller-03.js

--- taller-03.js
+++ taller-03.js
@@ -12,10 +12,33 @@ function desglosarString(st, opc) {
 }
 
 function twoSum(arr, num) {
+    // returns -1 in case of finding none
     for (let i = 0; i < arr.length; i++) {
-        let ind = arr.findIndex((x) => return x == num - arr[i])
-        if 
+        let ind = arr.findIndex((x) => {return x == num - arr[i]})
+        if (ind != -1) {
+            return [i, ind]
+        }
     }
+    return -1
 }
 
-console.log(twoSum([2,7,11,15],9))
+function conversionRomana(nr) {
+    let eq = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    let res = eq[nr[nr.length-1]]
+    for (let i = 0; i < nr.length-1; i++) {
+        if (eq[nr[i]] < eq[nr[i+1]]) {
+            res -= eq[nr[i]]
+        } else {
+            res += eq[nr[i]]
+        }
+    }
+    return res
+}
