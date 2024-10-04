4c0565012c41056d5cdb099b00df19eb01887b3b
paviag <98894987+paviag@users.noreply.github.com>
2024-10-04 18:22:09 -0500

    Update checkIfExists.users.action.ts

--- taller-04/src/users/checkIfExists.users.action.ts
+++ taller-04/src/users/checkIfExists.users.action.ts
@@ -1,8 +1,9 @@
-import { users } from "./users.model";
+import { UserType } from "./users.model";
 
 // DECLARE ACTION FUNCTION
 async function checkUserExistsAction(userId: number): Promise<boolean> {
-  const user = users.find(u => u.id == userId); //await UserModel.findById(userId);
+  let users: UserType[] = require('./23-taller-04-datos.json');
+  const user: UserType | undefined = users.find(u => u.id == userId); //await UserModel.findById(userId);
   
   return user != null;
 }
