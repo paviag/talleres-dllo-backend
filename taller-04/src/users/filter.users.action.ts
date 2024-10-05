0dea6cb2542220cadde825fd41a1fcaf5e9f2796
paviag <98894987+paviag@users.noreply.github.com>
2024-10-04 19:29:31 -0500

    Update filter.users.action.ts

--- taller-04/src/users/filter.users.action.ts
+++ taller-04/src/users/filter.users.action.ts
@@ -1,7 +1,8 @@
-import { users, UserType } from "./users.model";
+import { UserType } from "./users.model";
 
 // DECLARE ACTION FUNCTION
 async function filterUsersByFieldAction(field: string, value: string): Promise<UserType[]> {
+  const users: UserType[] = require('./23-taller-04-datos.json');
   const results = users.filter((u: UserType) => u[field as keyof UserType] == value) // await UserModel.find({ [field]: value });
 
   return results;
