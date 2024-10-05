f5c825f9b76dec78aac1fff3eb70ff39e18d2515
paviag <98894987+paviag@users.noreply.github.com>
2024-10-04 19:30:10 -0500

    Update filterFieldIncludes.users.action.ts

--- taller-04/src/users/filterFieldIncludes.users.action.ts
+++ taller-04/src/users/filterFieldIncludes.users.action.ts
@@ -1,8 +1,9 @@
-import { users, UserType } from "./users.model";
+import { UserType } from "./users.model";
 
 // DECLARE ACTION FUNCTION
 async function filterUsersByFieldIncludesAction(field: string, value: string): Promise<UserType[]> {
-  const results = users.filter((u: UserType) => {
+  const users: UserType[] = require('./23-taller-04-datos.json');
+  const results: UserType[] = users.filter((u: UserType) => {
     try {
       return (u[field as keyof UserType] as Array<string | number>).includes(value);
     } catch (e) {
